fee9d3477b5bc3a8ba330e89f317b43e053f7ba1
ronnika <lunkestr@gmail.com>
2022-08-08 02:53:22 +0300

    changed date format

--- src/main/java/italy/ItalyBot.java
+++ src/main/java/italy/ItalyBot.java
@@ -6,25 +6,31 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class ItalyBot {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         TelegramBot bot = new TelegramBot("5510060298:AAHkpXOLcnEbaw4-92DnE6JluoNwdbEUiyw");
         findWindow(bot);
     }
-    static void findWindow(TelegramBot bot) {
-        String date;
+    static void findWindow(TelegramBot bot) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        Date dateCurr = sdf.parse("19.09.2022");
+        Date date;
         Configuration.holdBrowserOpen = true;
         open("https://italy-vms.ru/autoform/?lang=ru");
         while (true) {
             Selenide.refresh();
             $("#center").selectOptionByValue("1");
             sleep(1000);
-            date = $("#free_date").getText();
+            date = sdf.parse($("#free_date").getText());
             sleep(1000);
-            if (date.contains("08.2022") || date.contains("09.2022")) {
+            if (date.compareTo(dateCurr) < 0) {
                 sleep(500);
                 SendMessage request = new SendMessage(-675415151, "Oкошко на " + date);
                 SendResponse sendResponse = bot.execute(request);
